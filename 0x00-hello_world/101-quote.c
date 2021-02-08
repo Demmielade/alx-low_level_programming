7ad9e4683a527b6813694762d16717ff95fa36c9
Demmielade <Yungjohn98@gmail.com>
2021-02-08 17:23:12 +0000

    you gotta be a genius to understand UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Description - Usage of other function name
+ *
+ *Return - zero on success 
+ */
+ int main(void)
+{
+	printf("and that piece of art is useful - Dora Korpar, 2015-10-19, /n");
+	return (0);
+}
