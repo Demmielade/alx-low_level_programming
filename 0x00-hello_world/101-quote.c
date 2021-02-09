5bb9150d32c2e77593096541b6aeee6926a6211b
Demmielade <Yungjohn98@gmail.com>
2021-02-09 15:14:06 +0000

    You gotta be a genius to understand UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - Prints quote to the standard error
+ * main - Prints quote
  *
  * Return: zero on success
  *
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that iece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
