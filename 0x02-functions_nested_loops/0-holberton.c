9bde7c1f025940e864108639435ec9569e120bb2
Demmielade <Yungjohn98@gmail.com>
2021-02-10 20:49:41 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Prints "Holberton" with _putchar
+ *
+ * Return - zero on success
+*/
+int main()
+{
+	putchar("Holberton,/n");
+	return (0);
+}
+
