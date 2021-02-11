530cf8b8ed10d5fb44132e4e8a0a406079538890
Oyetoro Johnson Demilade <71744303+Demmielade@users.noreply.github.com>
2021-02-11 17:22:35 +0100

    Positive anything > Negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,34 +1,40 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 
 /**
- * main - Entry point
+ * main - Positive or Negative - print sign of random number
+ * @void: Empty parameter list for main.
  *
- * Description - Print variable "n" has +ve or -ve number
+ * Description: this if/else statement declares the sign (positive
+ * or negative) of a random number. It also states if the value
+ * is zero.
  *
- * Return - zero on success
+ * Return: 0 for success
 */
 int main(void)
 {
   /**
-   *variable "n" has been declared
+   * variable n has been declared
+   * coding then assigns the random value.
    */
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;	
+	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
-	  printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-	  printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-	  printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
+
 	return (0);
 }
