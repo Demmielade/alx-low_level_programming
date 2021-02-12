07c4e3c2f3c43c2b226a50473032051730bd8859
Demmielade <Yungjohn98@gmail.com>
2021-02-12 12:24:05 +0000

    Alphabet game subdues insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - Prints all alphabets in lower case
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	putchar ([a-z]*);
+	putchar (\n);
+	return (0);
+}
+
