9a5f6c1b66924e994eafc0003022b45b099f3776
Oyetoro Johnson Demilade <71744303+Demmielade@users.noreply.github.com>
2021-02-14 08:35:06 +0100

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description - Prints all single digit(0-9) numbers of base 10 starting from 0
+ * Description - Prints all single digit(0-9) nos of base 10 starting from 0
  *
  * Return: 0 on success
  */
@@ -11,7 +11,7 @@ int main(void)
 {
 	char num = '0';
 
-	while (num <= '9') 
+	while (num <= '9')
 	{
 		putchar(num);
 		num++;
