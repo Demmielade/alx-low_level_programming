77fe78a3419ea97a8168571fe69745947ac83489
Demmielade <Yungjohn98@gmail.com>
2021-02-14 12:59:38 +0000

    Pending

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Prints the lower case alphabet in reverse
+ *
+ * Return: 0 on success
+ */
+int main(void)
+	char letter = 'a';
