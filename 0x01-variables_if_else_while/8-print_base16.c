e924e02752c2064370697ff459ec32c82f8a9032
Demmielade <Yungjohn98@gmail.com>
2021-02-14 21:02:48 +0000

    Hexadecimal: Prints all base 16 nnumbers in LOWERCASE using 'putchar thrice'

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Print all base 16 numbers in lower case
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int hex_num = '0';
+
+	while (hex_num <= '9')
+	{
+		putchar(hex_num);
+		hex_num++;
+	}
+
+	int hex_letter = 'a';
+
+	while (hex_letter <= 'f')
+	{
+		putchar(hex_letter);
+		hex_letter++;
+	}
+	putchar('\n');
+	return (0);
+}
